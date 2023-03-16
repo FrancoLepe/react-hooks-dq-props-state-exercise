3288a55ccb12ab27b0cb7eb9344e0d779def43de
Franco Lepe <Francolepe169@gmail.com>
2023-03-16 11:33:14 -0500

    done2

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import normalBaby from "../assets/unadulterated-hoglette.png";
 import SunBaby from "../assets/sun-eyes.png";
 import BlueBaby from "../assets/blue-eyes.png";
@@ -7,31 +7,50 @@ import GlowingBaby from "../assets/glowing-eyes.png";
 // is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
-function BabyHog(props) {
+function BabyHog({name,hobby,eyeColor}) {
+  
+ const eyeColorPicture = {
+  blue:BlueBaby,
+  sun:SunBaby,
+  glowing:GlowingBaby,
+
+ };
+  
+
+  const [weight,setWeight]=useState(10)
+  
+  
+
+  console.log(eyeColor)
+
   function handleChangeWeight(e) {
+    
+    (e.target.name==="+") ? setWeight(weight +10 ) : setWeight(weight -5 )
+      
+  
+   
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
   }
+  
+  //For an added bonus, use the weight of the BabyHog to dynamically change the height of the image
 
   return (
     <li className="hogbabies">
-      <h1>Name</h1>
-      <h3>Weight:</h3>
-      <h3>Hobby:</h3>
-      <h4>Eye Color:</h4>
+      <h1>{name}</h1>
+      <h3>Weight:{weight} lbs</h3>
+      <h3>{hobby}</h3>
+      <h4>Eye Color:{eyeColor}</h4>
 
-      <button name="+">Increase Weight</button>
-      <button name="-">Decrease Weight</button>
+      <button onClick={handleChangeWeight} name="+">Increase Weight</button>
+      <button  onClick={handleChangeWeight} name="-">Decrease Weight</button>
 
       <div className="hb-wrap">
-        <img
-          src={normalBaby}
-          style={{ height: "200px" }}
-          alt="MasterBlasterJrJr"
-        />
+        <img src={eyeColorPicture[eyeColor]} style={{ height: `${weight*5}px` }} alt="MasterBlasterJrJr"/>
       </div>
     </li>
   );
 }
 
 export default BabyHog;
+
